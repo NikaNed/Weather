cc5cb323b7c9d61f2277870493aea2d8899e6c85
NikaNed <babenkova.nika@ya.ru>
2022-06-24 16:54:40 +0300

    Create layouts

--- app/src/main/java/com/example/weather/presentation/WeatherActivity.kt
+++ app/src/main/java/com/example/weather/presentation/WeatherActivity.kt
@@ -3,10 +3,16 @@ package com.example.weather.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.weather.R
+import com.example.weather.databinding.ActivityWeatherBinding
 
 class WeatherActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityWeatherBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
